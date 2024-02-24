ad5fdbe0052f2b8bc51e2544084f0df60181fda9
Kevin Won <kevin.won91@gmail.com>
2024-02-23 21:55:13 -0600

    100vh and add onclick href to sections| need: socials component and carousel

---
+++ src/photos/scripts.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+
+// Updated list of files
+const files = [
+  'gitWorkshop_1.jpg',
+  'gitWorkshop_2.jpg',
+  'gitWorkshop_3.jpg',
+  'gitWorkshop_4.jpg',
+  'gitWorkshop_5.jpg',
+  'gitWorkshop_6.jpg',
+  'gitWorkshop_7.jpg',
+  'gitWorkshop_8.jpg',
+  'gitWorkshop_9.jpg',
+  'gitWorkshop_10.jpg',
+]
+
+// Function to rename files with "022024" and leading '0' for single digits
+const renameFilesWithInsertion = () => {
+  files.forEach((file, index) => {
+    const paddedIndex = index < 9 ? `0${index + 1}` : `${index + 1}` // Add leading '0' for single digits
+    const newFileName = file.replace(
+      /gitWorkshop_(\d+)\.jpg$/,
+      `gitWorkshop_022024_${paddedIndex}.jpg`,
+    )
+    fs.rename(file, newFileName, (err) => {
+      if (err) throw err
+      console.log(`${file} => ${newFileName}`)
+    })
+  })
+}
+
+// Execute the rename function
+renameFilesWithInsertion()
